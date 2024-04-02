91c3669f5d5af578327aeed03b78b1676dc01cf6
niteshjha12 <119284030+niteshjha12@users.noreply.github.com>
2024-04-02 13:28:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.6.3"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.91.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
